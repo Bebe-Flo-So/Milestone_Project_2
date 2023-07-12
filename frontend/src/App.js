5afd992d16ab9f395111e946c8c32f218fb29075
kmumfor2 <120679405+kmumfor2@users.noreply.github.com>
2023-07-11 20:31:07 -0400

    added API,home page template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react'
+
+
 
 function App() {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YWE5YjE2ODcxN2RiMjE3NDEzZDQzODdhOTFhZWY2YyIsInN1YiI6IjY0YWRlYTAwNmEzNDQ4MDBjOWZjZTJkNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FJeABoW3VzJusjQMyBC_Im__J0eNyOhMeBKCZ-UaDA0'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/authentication', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+        <header>
+          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+
+          <div className="navBar">
+            
+          </div>
+        </header>
+
+        <div className="display">
+          
+        </div>
+
+  
+
     </div>
   );
 }
